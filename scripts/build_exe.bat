ffe34b8688b0b32042d8a8b31f3b608d4fa22200
falker47 <maurizio.falconi47@gmail.com>
2026-01-07 10:22:51 +0100

    improve buildexe.bat

--- scripts/build_exe.bat
+++ scripts/build_exe.bat
@@ -3,13 +3,17 @@ echo Installing dependencies...
 cd /d "%~dp0.."
 pip install -r "requirements.txt"
 if %ERRORLEVEL% NEQ 0 (
+    echo Error using pip directly. Trying with python -m pip...
+    python -m pip install -r "requirements.txt"
+)
+if %ERRORLEVEL% NEQ 0 (
     echo Error installing dependencies.
     pause
     exit /b %ERRORLEVEL%
 )
 
 echo Building Panacea EXE...
-pyinstaller --noconsole --onefile --name "Panacea" --icon="assets\panacea_icon.ico" --add-data "assets;assets" --clean --collect-all customtkinter main.py --distpath "dist" --workpath "build" --specpath .
+python -m PyInstaller --noconsole --onefile --name "Panacea" --icon="assets\panacea_icon.ico" --add-data "assets;assets" --clean --collect-all customtkinter main.py --distpath "dist" --workpath "build" --specpath .
 echo.
 echo Build complete. Check the 'dist' folder for Panacea.exe
 pause
